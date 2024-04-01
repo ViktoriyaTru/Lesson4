c297b7583e376fccb00b988bdda56a2364e6b7aa
ViktoriyaTru <viktoriya.trubitsina@gmail.com>
2024-04-01 14:03:46 +0300

    Lesson4; add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿string Getletters(string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+string str = Console.ReadLine();
+string letters = Getletters(str);
+Console.WriteLine(letters);
